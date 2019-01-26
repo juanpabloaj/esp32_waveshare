0783fc18c6cf0390d306ec1ee5b64ade50bdc7b2
JuanPablo <jpabloaj@gmail.com>
2019-01-26 20:32:58 -0300

    added date with ntp

---
+++ 02_date_with_ntp/02_date_with_ntp.ino
@@ -0,0 +1,66 @@
+#include<NTPClient.h>
+#include<WiFi.h>
+#include<WiFiUdp.h>
+
+#include <GxEPD.h>
+#include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w
+#include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
+#include <GxIO/GxIO.cpp>
+#include <Fonts/FreeSansBold12pt7b.h>
+
+const char *ssid = "ssid";
+const char *password = "password";
+
+WiFiUDP ntpUDP;
+
+NTPClient timeClient(ntpUDP);
+
+GxIO_Class io(SPI, SS, 22, 21);
+GxEPD_Class display(io, 16, 4);
+
+const char* name = "FreeSansBold12pt7b";
+const GFXfont* f = &FreeSansBold12pt7b;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting ...");
+
+  display.init(9600);
+  display.setRotation(1);
+  display.fillScreen(GxEPD_BLACK);
+  display.setFont(f);
+  display.update();
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("Connecting to wifi ...");
+  }
+
+  timeClient.begin();
+}
+
+void loop() {
+  showDateTime();
+  delay(10000);
+}
+
+void showDateTime() {
+  timeClient.update();
+
+  String dateString = timeClient.getFormattedTime();
+  Serial.println(dateString);
+
+  uint16_t box_x = 40;
+  uint16_t box_y = 40;
+  uint16_t box_w = 110;
+  uint16_t box_h = 24;
+  uint16_t cursor_y = box_y + 20;
+
+  display.fillRect(box_x, box_y, box_w, box_h, GxEPD_BLACK);
+  display.setTextColor(GxEPD_WHITE);
+  display.setCursor(box_x+4, cursor_y);
+  display.print(dateString);
+  display.updateWindow(box_x, box_y, box_w, box_h, true);
+}
--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 * https://www.youtube.com/watch?v=1xQqc6ZCXdw
 * https://educ8s.tv/2-9-e-paper-display/
 
+### 02 date with ntp
+
+* https://randomnerdtutorials.com/esp32-ntp-client-date-time-arduino-ide/
+
 ### Libraries
 
 * https://github.com/ZinggJM/GxEPD
+* https://github.com/arduino-libraries/NTPClient
