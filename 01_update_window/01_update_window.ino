fca6fb2becd0dedf66fc3b6fb9d5796757436f27
JuanPablo <jpabloaj@gmail.com>
2020-01-05 11:25:43 -0300

    added epaper's pin connectors numbers

--- 01_update_window/01_update_window.ino
+++ 01_update_window/01_update_window.ino
@@ -1,10 +1,21 @@
 
 #include <GxEPD.h>
-#include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w
-#include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
-#include <GxIO/GxIO.cpp>
+#include <GxGDEP015OC1/GxGDEP015OC1.h>    // 1.54" b/w
+//#include <GxGDEH029A1/GxGDEH029A1.h>      // 2.9" b/w
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <GxIO/GxIO.h>
 #include <Fonts/FreeSansBold12pt7b.h>
 
+/*
+BUSY  4
+RST   21
+DC    22
+CS    5
+CLK   18
+DIN   23
+GND   GND
+3.3V  3.3V
+*/
 
 GxIO_Class io(SPI, SS, 22, 21);
 GxEPD_Class display(io, 16, 4);
@@ -14,9 +25,9 @@ const GFXfont* f = &FreeSansBold12pt7b;
 
 void setup() {
   randomSeed(analogRead(0));
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Starting...");
-  display.init(9600);
+  display.init(115200);
   display.setRotation(1);
   display.fillScreen(GxEPD_BLACK);
   display.setFont(f);
@@ -25,6 +36,7 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("loop ...");
   showRandomNumber();
   delay(1000);
 }
